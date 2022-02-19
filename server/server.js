1f0c399ccec723345fec3393bd328f373511fc01
Dan <drblack651@gmail.com>
2022-02-18 19:55:47 -0600

    overwrite default express error handler

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware'); 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler);    // overwrites default express error handler
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
